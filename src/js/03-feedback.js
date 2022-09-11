bc5c9580a64a47228a23fd81675106ab7639f687
Bilous Ruslan <bilousr0426@gmail.com>
2022-09-11 21:06:31 +0300

    home-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const from = document.querySelector('feedback-form');
+from.addEventListener('input', throttle(onFormData, 500));
+from.addEventListener('submit', onSubmitFrom);
+
+const onFormData = {};
+
+function onFormData(e) {
+  FormData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+function onSabmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
